fcbb0d46d23fb98640a2a0fede91715ad228bcae
dineRiyad <asmdine1012@gmail.com>
2024-02-21 19:50:54 +0100

    completing the functions in the js file and updating some html and css content

--- script.js
+++ script.js
@@ -4,19 +4,36 @@ const resultsDiv = document.getElementById("results-div");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 
+const fitstReg = /^1?\s?\(\d{3}\)\s?\d{3}\s?-?\d{4}$/;
+const secondReg = /^1?\s?\d{3}[-\s]?\d{3}[-\s]?\d{4}$/;
+const regexArray = [fitstReg, secondReg];
+
+const results = (input) => {
+  if (regexArray.some((regex) => regex.test(input))) {
+    resultsDiv.innerHTML += `<p class="valid-input font">Valid US number: ${input}</p>`;
+  } else {
+    resultsDiv.innerHTML += `<p class="invalid-input font">Invalid US number: ${input}</p>`;
+  }
+};
+
 const isEmptyOrInvalid = (value) => {
   value = userInput.value;
 
   if (!value) {
     alert("Please provide a phone number");
   } else {
-    console.log(value);
+    results(value);
+    userInput.value = "";
   }
 };
 
+validationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
 userInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    isEmptyOrInvalid;
+    isEmptyOrInvalid();
   }
 });
 
@@ -24,9 +41,4 @@ checkBtn.addEventListener("click", isEmptyOrInvalid);
 
 clearBtn.addEventListener("click", () => {
   resultsDiv.innerHTML = "";
-});
-
-validationForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  isEmptyOrInvalid();
 });
